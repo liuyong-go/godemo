ba1bfb139680695b83c90f1d17f9b026af897122
liuyong <liuyong@soyoung.com>
2021-03-04 19:52:11 +0800

    init

---
+++ socket/go.mod
@@ -0,0 +1,5 @@
+module github.com/liuyong-go/godemo/socket
+
+go 1.13
+
+replace github.com/liuyong-go/godemo/socket => /Users/liuyong/go/src/godemo/socket
---
+++ socket/server/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/liuyong-go/godemo/socket/server/yong"
+)
+
+// HelloServer world, the web server
+func HelloServer(w http.ResponseWriter, req *http.Request) {
+	cxt := context.Background()
+
+	fmt.Println(cxt)
+	io.WriteString(w, "hello, world!\n")
+}
+
+func main() {
+	err := yong.ListenAndServe(":12345")
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
+
+type testString struct {
+	Name string
+}
+
+func (t *testString) String() string {
+	return "get teststring" + t.Name
+}
